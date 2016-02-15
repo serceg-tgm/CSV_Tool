afd116f794793402c7ca163f882609afbe7cce9a
serceg-tgm <serceg@student.tgm.ac.at>
2016-02-15 22:37:12 +0100

    second try to push my 5 awesome unit tests :)

--- CSVReader.py
+++ CSVReader.py
@@ -1,1 +1,26 @@
-__author__ = 'Stipe'
+from csv import Sniffer, DictReader
+
+class CSVReader:
+
+    @staticmethod
+    def read(filename):
+
+        with open(filename, "r") as csvfile:
+
+            sniffer = Sniffer()
+            sample = csvfile.read(1024)
+            dialect = sniffer.sniff(sample, delimiters=[';', ','])
+
+            if sniffer.has_header(sample):
+                # file has header
+                pass
+
+            csvfile.seek(0)
+
+            lines_reader = DictReader(csvfile, dialect=dialect)
+
+            lines = []
+            for line in lines_reader:
+                lines.append(line)
+
+            return lines
--- CSVTool_Tests.py
+++ CSVTool_Tests.py
@@ -1,1 +1,25 @@
-__author__ = 'Stipe'
+from unittest import TestCase
+from CSVReader import CSVReader
+
+csvFile = "RealEstateTransactions.csv"
+
+class CSVTool_Tests(TestCase):
+
+    def testFileNotFoundError(self):
+        self.assertRaises(FileNotFoundError, CSVReader.read, "kleiner Test...")
+
+    def testFirstLineOfFile(self):
+        lines = CSVReader.read(csvFile)
+        self.assertEqual("5601 REXLEIGH DR", lines[0]["street"])
+
+    def testSecondLineOfFile(self):
+        lines = CSVReader.read(csvFile)
+        self.assertEqual("1909 YARNELL WAY", lines[1]["street"])
+
+    def testThirdLineOfFile(self):
+        lines = CSVReader.read(csvFile)
+        self.assertEqual("9169 GARLINGTON CT", lines[2]["street"])
+
+    def testFourthLineOfFile(self):
+        lines = CSVReader.read(csvFile)
+        self.assertEqual("6932 RUSKUT WAY", lines[3]["street"])
